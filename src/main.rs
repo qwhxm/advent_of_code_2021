67d93df3e903ce4801426276ef778a351b2864a4
Marek Tomáštík <tomastik.marek@gmail.com>
2021-12-03 00:42:06 +0100

    feat: Add d2 s1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-mod d1;
+#![allow(dead_code)] // Because all modules except the one used in main() are dead code.
 
-use d1::{solution_1, solution_2};
+mod d1;
+mod d2;
+
+use d2::{solution_1, solution_2};
 
 fn main() {
-    println!("{}", solution_1());
-    println!("{}", solution_2());
+    println!("Part 1 solution: {}", solution_1());
+    println!("Part 2 solution: {}", solution_2());
 }

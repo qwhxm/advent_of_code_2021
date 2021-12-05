6bdb48b59cece9bc9cf93422278b69956f055944
Marek Tomáštík <tomastik.marek@gmail.com>
2021-12-05 15:16:05 +0100

    refactor: Rename modules dX -> dayX

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #![allow(dead_code)] // Because all modules except the one used in main() are dead code.
 
-mod d1;
-mod d2;
-mod d3;
+mod day1;
+mod day2;
+mod day3;
 
-use d3::{solution_1, solution_2};
+use day3::{solution_1, solution_2};
 
 fn main() {
     println!("Part 1 solution: {}", solution_1());

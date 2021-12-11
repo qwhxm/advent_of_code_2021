240be5fcd25e505f4ab9987c0e093428c8ea83d3
Marek Tomáštík <marek.tomastik2@firma.seznam.cz>
2021-12-11 01:25:14 +0100

    feat: Add d9

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,9 @@ mod day5;
 mod day6;
 mod day7;
 mod day8;
+mod day9;
 
-use day8::{solution_1, solution_2};
+use day9::{solution_1, solution_2};
 
 fn main() {
     println!("Part 1 solution: {}", solution_1());

38e14a7b120fd8b37abd5e17fc3bf153f015e54a
Marek Tomáštík <tomastik.marek@gmail.com>
2021-12-16 22:02:05 +0100

    feat: Add d15

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,9 @@ mod day11;
 mod day12;
 mod day13;
 mod day14;
+mod day15;
 
-use day14::{solution_1, solution_2};
+use day15::{solution_1, solution_2};
 
 fn main() {
     println!("Part 1 solution: {}", solution_1());
